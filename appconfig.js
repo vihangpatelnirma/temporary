0401fa37190b40cfc90454dacb89373914fe4a11
Vihang Patel <vihang.engg@gmail.com>
2018-06-25 21:44:53 +0530

    Change header

--- appconfig.js
+++ appconfig.js
@@ -6,10 +6,11 @@ define('appconfig' ,[] , function() {
             { text : 'Home' , url : '/', subMenu : [], firstUrl : '/'} ,            
             { text : 'Types Of Dances' , url : 'dances', subMenu : [], firstUrl : '/dances'},
             { text : 'Services' , url : 'services', subMenu : [], firstUrl : '/services'},
-            { text : 'About Us' , url : 'about-us', subMenu : [], firstUrl : '/about-us'}
+            { text : 'About Us' , url : 'about-us', subMenu : [], firstUrl : '/about-us'},
             { text : 'Contact Us' , url : 'contact-us', subMenu : [], firstUrl : '/contact-us'}
         ],
-        FOOTER : []
+        FOOTER : [],
+        REVIEWS: []
     }
 
 
