ae3dfc567791fded1f142ca3a12f906d18ab5cb2
vihangpatelnirma <vihang.engg@gmail.com>
2018-06-26 09:28:19 +0530

    Show site under construction message

--- appconfig.js
+++ appconfig.js
@@ -3,10 +3,7 @@ define('appconfig' ,[] , function() {
 
     return {
         HEADER :  [
-            { text : 'Home' , url : '/', subMenu : [], firstUrl : '/'} ,            
-            { text : 'Types Of Dances' , url : 'dances', subMenu : [], firstUrl : '/dances'},
-            { text : 'Services' , url : 'services', subMenu : [], firstUrl : '/services'},
-            { text : 'About Us' , url : 'about-us', subMenu : [], firstUrl : '/about-us'},
+            { text : 'Home' , url : '/', subMenu : [], firstUrl : '/'} ,                       
             { text : 'Contact Us' , url : 'contact-us', subMenu : [], firstUrl : '/contact-us'}
         ],
         FOOTER : [],
